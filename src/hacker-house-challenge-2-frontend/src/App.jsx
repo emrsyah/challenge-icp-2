8f97bc126ce38dd4c847195ea6ab9ebab0493443
Muhammad Emirsyah M <muhammademir48@gmail.com>
2024-08-21 17:32:25 +0700

    Initial commit.

---
+++ src/hacker-house-challenge-2-frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { hacker_house_challenge_2_backend } from 'declarations/hacker-house-challenge-2-backend';
+
+function App() {
+  const [greeting, setGreeting] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const name = event.target.elements.name.value;
+    hacker_house_challenge_2_backend.greet(name).then((greeting) => {
+      setGreeting(greeting);
+    });
+    return false;
+  }
+
+  return (
+    <main>
+      <img src="/logo2.svg" alt="DFINITY logo" />
+      <br />
+      <br />
+      <form action="#" onSubmit={handleSubmit}>
+        <label htmlFor="name">Enter your name: &nbsp;</label>
+        <input id="name" alt="Name" type="text" />
+        <button type="submit">Click Me!</button>
+      </form>
+      <section id="greeting">{greeting}</section>
+    </main>
+  );
+}
+
+export default App;
