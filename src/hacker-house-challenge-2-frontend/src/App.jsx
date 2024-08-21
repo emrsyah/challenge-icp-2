af331e47015bf0358159cda631514b6495d8aded
Muhammad Emirsyah M <muhammademir48@gmail.com>
2024-08-21 18:29:13 +0700

    first commit

--- src/hacker-house-challenge-2-frontend/src/App.jsx
+++ src/hacker-house-challenge-2-frontend/src/App.jsx
@@ -3,19 +3,31 @@ import { hacker_house_challenge_2_backend } from 'declarations/hacker-house-chal
 
 function App() {
   const [greeting, setGreeting] = useState('');
-
+  const [firstData, setFirstData] = useState("")
+  const [age, setAge] = useState(null)
   function handleSubmit(event) {
     event.preventDefault();
-    const name = event.target.elements.name.value;
-    hacker_house_challenge_2_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
+    hacker_house_challenge_2_backend.predict_age(firstData).then((greeting) => {
+      const parsed = (JSON.parse(greeting))
+      console.log(parsed)
+      setAge(parsed.age);
     });
     return false;
   }
 
   return (
     <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
+      <h1>Chart Maker</h1>
+      <div>
+        <h1>Your Age By Your Name</h1>
+        <h5>Your Name:</h5>
+        <input type="name" onChange={(ev) => setFirstData(ev.target.value)} />
+        <button onClick={handleSubmit}>Cek My Age</button>
+      </div>
+      <div>
+        Your Age: {age}
+      </div>
+      {/* <img src="/logo2.svg" alt="DFINITY logo" />
       <br />
       <br />
       <form action="#" onSubmit={handleSubmit}>
@@ -23,7 +35,7 @@ function App() {
         <input id="name" alt="Name" type="text" />
         <button type="submit">Click Me!</button>
       </form>
-      <section id="greeting">{greeting}</section>
+      <section id="greeting">{greeting}</section> */}
     </main>
   );
 }
